d3e53466b88885375d5ab21400218dbfdaad6e0d
lepeshichev <lepeshichev>
2022-06-21 14:01:41 +0300

    1.Базовый синтаксис

--- base-syntax/src/ru/sber/base/syntax/Task5.java
+++ base-syntax/src/ru/sber/base/syntax/Task5.java
@@ -1,27 +1,23 @@
 package ru.sber.base.syntax;
 
-// в промежутке [2, 100]. Используйте для решения этой задачи оператор % (остаток от деления) и циклы.
+//Напишите программу, которая выводит на консоль простые числа в промежутке [2, 100].
+// Используйте для решения этой задачи оператор % (остаток от деления) и циклы.
 public class Task5 {
     public static void main(String[] args) {
-        int[] numbers = new int[10];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int) (Math.random() * 99 + 2);
+        for (int i = 2; i < 101; i++) {
+            if(isPrime(i))
+                System.out.print(i + " ");
         }
-        for(int each : numbers)
-            System.out.print(each + " ");
-        int max = Integer.MIN_VALUE;
-        for (int each : numbers) {
-            if (each > max)
-                max = each;
+    }
+
+    private static boolean isPrime(int number){
+        boolean isPrime = true;
+        for(int i = 2; i < number; i++){
+            if(number % i == 0){
+                isPrime = false;
+                break;
+            }
         }
-        System.out.println();
-        System.out.println(max);
-        int sum = 0;
-        int counter = 0;
-        for (int each : numbers) {
-            sum += each;
-            counter++;
-        }
-        System.out.println(sum / counter);
+        return isPrime;
     }
 }
