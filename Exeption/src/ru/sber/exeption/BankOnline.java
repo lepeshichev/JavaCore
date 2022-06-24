908462b832397a2b0b6fc463a79ecfb85b64e0a8
lepeshichev <lepeshichev>
2022-06-24 11:54:02 +0300

    4. Исключения

--- Exeption/src/ru/sber/exeption/BankOnline.java
+++ Exeption/src/ru/sber/exeption/BankOnline.java
@@ -3,58 +3,34 @@ package ru.sber.exeption;
 import java.io.*;
 
 public class BankOnline {
-    public void send(String cardNumber, Double money) throws IOException {
+    public void send(String cardNumber, Double money) throws IOException, NullSendArgumentsExeption, InvalidCardNumberExeption, BlockedCardExeption, InvalidTransferAmountExeption, TransferLimitExceededExeption {
         boolean success = true;
         if (cardNumber == null  || money == null) {
-            try {
-                throw new NullSendArgumentsExeption("Проверьте ввод данных");
-            } catch (NullSendArgumentsExeption e) {
-                e.printStackTrace();
-                return;
-            }
-
+            throw new NullSendArgumentsExeption("Проверьте ввод данных");
         }
 
         if(cardNumber.replaceAll(" ", "").length() != 16) {
-            try {
+            success = false;
+            throw new InvalidCardNumberExeption("Неверный номер карты");
+        }
+        BufferedReader reader = new BufferedReader(new FileReader("cards.txt"));
+        String line = reader.readLine();
+        while (line != null) {
+            if (line.equals(cardNumber)) {
                 success = false;
-                throw new InvalidCardNumberExeption("Неверный номер карты");
-            } catch (InvalidCardNumberExeption e) {
-                e.printStackTrace();
+                throw new BlockedCardExeption("Перевод невозможен. Карта заблокирована");
             }
-        }
-        try(BufferedReader reader = new BufferedReader(new FileReader("cards.txt"))) {
-            String line = reader.readLine();
-            while (line != null) {
-                if (line.equals(cardNumber)) {
-                    success = false;
-                    throw new BlockedCardExeption("Перевод невозможен. Карта заблокирована");
-                }
-                line = reader.readLine();
-            }
-        }
-        catch (BlockedCardExeption e) {
-            e.printStackTrace();
+            line = reader.readLine();
         }
         if (money < 0) {
-            try {
-                success = false;
-                throw new InvalidTransferAmountExeption("Введена отрицательная сумма перевода");
-            } catch (InvalidTransferAmountExeption e) {
-                e.printStackTrace();
-            }
+            success = false;
+            throw new InvalidTransferAmountExeption("Введена отрицательная сумма перевода");
         }
         if (money > 50_000.0) {
             success = false;
-            try {
-                throw new TransferLimitExceededExeption("Превышен лимит в 50 тыс.");
-            } catch (TransferLimitExceededExeption e) {
-                e.printStackTrace();
-            }
+            throw new TransferLimitExceededExeption("Превышен лимит в 50 тыс.");
         }
-
         if(success)
             System.out.println("Операция проведена успешно");
     }
-
 }
--- Exeption/src/ru/sber/exeption/BankOnlineTest.java
+++ Exeption/src/ru/sber/exeption/BankOnlineTest.java
@@ -5,12 +5,24 @@ import java.io.IOException;
 public class BankOnlineTest {
     public static void main(String[] args) throws IOException {
         BankOnline bo = new BankOnline();
-        bo.send("1234 1234 4123 1234",10.0);
-        bo.send("1234 1234 4123 123",10.0);
-        bo.send("3333 3333 3333 3333",10.0);
-        bo.send("1234 1234 4123 1234",-1.0);
-        bo.send("1234 1234 4123 1234", 50_000.1);
-        bo.send("1234 1234 4123 1234", null);
+        try {
+            bo.send("1234 1234 4123 1234",10.0);
+            //bo.send("1234 1234 4123 123",10.0);
+            //bo.send("3333 3333 3333 3333",10.0);
+            bo.send("1234 1234 4123 1234",-1.0);
+            //bo.send("1234 1234 4123 1234", 50_000.1);
+            //bo.send(null, null);
+        } catch (NullSendArgumentsExeption e) {
+            e.printStackTrace();
+        } catch (InvalidCardNumberExeption e) {
+            e.printStackTrace();
+        } catch (BlockedCardExeption e) {
+            e.printStackTrace();
+        } catch (InvalidTransferAmountExeption e) {
+            e.printStackTrace();
+        } catch (TransferLimitExceededExeption e) {
+            e.printStackTrace();
+        }
     }
 
 }
